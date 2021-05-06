27532a0b60561572db9dc9cc36465a3adb4f7c32
varung2 <varung2@illinois.edu>
2021-05-06 18:57:24 -0500

    tests

--- modified.py
+++ modified.py
@@ -1,5 +1,22 @@
 import config
+import torch
 
-splatting_renderer = create_splatting_renderer()
+from DSS.core.cloud import PointClouds3D
 
-print('Splatting renderer type: {}'.format(type(splatting_renderer)))
+if __name__ == '__main__':
+
+	# testing the splatting renderer
+	splatting_renderer = config.create_splatting_renderer()
+	print('Splatting renderer type: {}'.format(type(splatting_renderer)))
+
+	rand_pts_pos = torch.randn((1, 1000, 3));
+	rand_pts_feat = torch.randn((1, 1000, 10));
+	print('DEBUG: rand_pts_pos shape: {}'.format(rand_pts_pos.shape))
+	print('DEBUG: rand_pts_feat shape: {}'.format(rand_pts_feat.shape))
+
+	pt_cloud = PointClouds3D(points=rand_pts_pos, features=rand_pts_feat)
+	print('Point cloud type: {}'.format(type(pt_cloud)))
+
+	images = splatting_renderer(pt_cloud)
+	print('Images type: {}'.format(type(images)))
+	print('Images shape: {}'.format(images.shape))
